1c818eab27f90de6414a9e579c8f65ff82e5c726
phuchauxd12 <118866081+phuchauxd12@users.noreply.github.com>
2025-10-25 01:51:22 +1100

    feat: repository

---
+++ backend/src/main/java/NeonSquare/backend/repositories/UserRepository.java
@@ -0,0 +1,11 @@
+package NeonSquare.backend.repositories;
+
+import NeonSquare.backend.models.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.UUID;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, UUID> {
+}
