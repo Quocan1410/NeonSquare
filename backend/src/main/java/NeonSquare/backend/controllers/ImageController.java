5c0063818983f730d9b8378b2b40927e01bc7728
phuchauxd12 <118866081+phuchauxd12@users.noreply.github.com>
2025-10-25 01:48:32 +1100

    feat: upload image

---
+++ backend/src/main/java/NeonSquare/backend/controllers/ImageController.java
@@ -0,0 +1,39 @@
+package NeonSquare.backend.controllers;
+
+import NeonSquare.backend.models.Image;
+import NeonSquare.backend.services.ImageService;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+import java.util.UUID;
+
+@RestController
+@RequestMapping("/api/images")
+public class ImageController {
+
+    private final ImageService imageService;
+
+    public ImageController(ImageService imageService) {
+        this.imageService = imageService;
+    }
+
+    @PostMapping("/upload")
+    public ResponseEntity<String> uploadImage(@RequestParam("file") MultipartFile file) throws IOException {
+        Image saved = imageService.saveImage(file);
+        return ResponseEntity.ok("Uploaded image with ID: " + saved.getId());
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<byte[]> getImage(@PathVariable UUID id) {
+        Image image = imageService.getImage(id);
+
+        return ResponseEntity.ok()
+                .header(HttpHeaders.CONTENT_DISPOSITION, "inline; filename=\"" + image.getName() + "\"")
+                .contentType(MediaType.APPLICATION_OCTET_STREAM)
+                .body(image.getData());
+    }
+}
